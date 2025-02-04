6aae715a7893427c698cd3de9d6fdb127daac855
Kai De La Cruz <kaidcruz@gmail.com>
2025-02-04 13:27:55 -0800

    Add files via upload

--- QArmForwardKinematics.m
+++ QArmForwardKinematics.m
@@ -1,4 +1,4 @@
-function T04 = QArmForwardKinematic(theta1, theta2, theta3, theta4)
+function T04 = QArmForwardKinematics(theta1, theta2, theta3, theta4)
 
 % dims in meters
 L1 = 0.14;
@@ -8,23 +8,6 @@ L4 = 0.25;
 L5 = 0.15;
 beta = atan2(L3,L2);
 
-syms theta d a alp 
-
-T = [cos(theta) -sin(theta)*cos(alp) sin(theta)*sin(alp) a*cos(theta);
-    sin(theta) cos(theta)*cos(alp) -cos(theta)*sin(alp) a*sin(theta);
-    0 sin(alp) cos(alp) d;
-    0 0 0 1];
-
-T01 = subs(T, [theta, d, a, alp], [theta1, L1, 0, -pi/2]);
-
-T12 = subs(T, [theta, d, a, alp], [(beta-pi/2)+theta2, 0, sqrt(L2^2 + L3^2), 0]);
-
-T23 = subs(T, [theta, d, a, alp], [-beta+theta3, 0, 0, -pi/2]);
-
-T34 = subs(T, [theta, d, a, alp], [theta4, L4+L5, 0, 0]);
-
-T04 = T01*T12*T23*T34;
-
+T04 = [sin(theta1)*sin(theta4) + cos(theta1)*cos(theta2)*cos(theta4)*sin(theta3) + cos(theta1)*cos(theta3)*cos(theta4)*sin(theta2), cos(theta4)*sin(theta1) - cos(theta1)*cos(theta2)*sin(theta3)*sin(theta4) - cos(theta1)*cos(theta3)*sin(theta2)*sin(theta4), cos(theta2 + theta3)*cos(theta1), cos(theta2 + theta3)*cos(theta1)*(L4 + L5) + sin(beta + theta2)*cos(theta1)*sqrt(L2^2 + L3^2); cos(theta2)*cos(theta4)*sin(theta1)*sin(theta3) - cos(theta1)*sin(theta4) + cos(theta3)*cos(theta4)*sin(theta1)*sin(theta2), - cos(theta1)*cos(theta4) - cos(theta2)*sin(theta1)*sin(theta3)*sin(theta4) - cos(theta3)*sin(theta1)*sin(theta2)*sin(theta4), cos(theta2 + theta3)*sin(theta1), sin(beta + theta2)*sin(theta1)*sqrt(L2^2 + L3^2) + cos(theta2 + theta3)*sin(theta1)*(L4 + L5); cos(theta2 + theta3)*cos(theta4), -cos(theta2 + theta3)*sin(theta4), -sin(theta2 + theta3), L1 + cos(beta + theta2)*sqrt(L2^2 + L3^2) - L4*sin(theta2 + theta3) - L5*sin(theta2 + theta3); sym(0), sym(0), sym(0), sym(1)] 
 end
 
-T04 = simplify(QArmForwardKinematic(0, 0, 0, 0))
